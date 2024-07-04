f44852e67523476e9204cf69c63788a200b39e1a
Hpavic <hrvoje.pavic994@gmail.com>
2024-07-04 11:50:14 +0200

    new project tipster hub
    new project tipster hub

--- src/constants/skillIcons.js
+++ src/constants/skillIcons.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHtml5, faCss3Alt, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5, faCss3Alt, faJsSquare, faReact, faNodeJs, faNpm } from '@fortawesome/free-brands-svg-icons';
 
 const skillIcons = {
   HTML: { icon: <FontAwesomeIcon icon={faHtml5} />, name: "HTML", color: "#e44d26" },
@@ -11,7 +11,12 @@ const skillIcons = {
   ASPNETCore: { icon: <div className="asp-net-background" />, name:"ASP.NET Core", color:"#c4c4c4" },
   EntityFramework: { name: "Entity Framework", color: "#a4a4a4"},
   MVC: { name: "MVC", color: "#b4b4b4" },
-  RazorPages: { name: "Razor Pages", color: "#d4d4d4"}
+  RazorPages: { name: "Razor Pages", color: "#d4d4d4"},
+  Redux: { icon: <FontAwesomeIcon icon={faReact} />, name: "Redux", color: "#764abc" },
+  NodeJS: { icon: <FontAwesomeIcon icon={faNodeJs} />, name: "Node.js", color: "#68a063" },
+  Express: { icon: <FontAwesomeIcon icon={faNpm} />, name: "Express", color: "#ffffff" },
+  Puppeteer: { name: "Puppeteer", color: "#40B5A4" },
+  HtmlAgilityPack: { name: "HtmlAgilityPack", color: "#e34f26" },
 };
 
 export default skillIcons;
