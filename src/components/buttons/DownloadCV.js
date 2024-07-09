11a5cb4ebe8e09e355bd3b6549b37679dec5e874
Hpavic <hrvoje.pavic994@gmail.com>
2024-07-09 13:34:58 +0200

    cv eng added
    cv eng added

--- src/components/buttons/DownloadCV.js
+++ src/components/buttons/DownloadCV.js
@@ -3,12 +3,18 @@ import { useLanguage } from '../../LanguageContext';
 import './DownloadCV.css';
 
 const DownloadCV = () => {
-    const { translate } = useLanguage();
+    const { translate, language } = useLanguage();
+    
+    const croId = "1ceOmGR9hq8sVqu0Hra8iaLjIzEfpezAA";
+    const engId = "1JFpvjJSvech_ey8B6Mj21sIIgHNWlxXB";
+    
+    const fileId = language === 'hr' ? croId : engId;
+    const fileName = language === 'hr' ? "CV_Hrvoje_Pavic_HR.pdf" : "CV_Hrvoje_Pavic_EN.pdf";
 
     return (
         <div className="download-cv-button-container">
-            <a href="https://drive.google.com/uc?export=download&id=1Qz3C7Yvqw0Ck5NaAm4Msh5ygBs1YL3y6"
-               download="CV_Hrvoje_Pavic.pdf"
+            <a href={`https://drive.google.com/uc?export=download&id=${fileId}`}
+               download={fileName}
                className="button-download-cv"
                title={translate('btnCV')}
                aria-label={translate('btnCV')}>
